4acde8e7235949a01443ec75c2dd1cd1e90735e6
Jonibek <94430009+MJony05@users.noreply.github.com>
2023-07-31 16:35:30 +0500

    Product Component, Functionality & Responsive

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			issuer: /\.[jt]sx?$/,
-			use: ['@svgr/webpack'],
-		});
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['media.graphassets.com'],
+    dangerouslyAllowSVG: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
 
-		return config;
-	},
-};
+    return config
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
