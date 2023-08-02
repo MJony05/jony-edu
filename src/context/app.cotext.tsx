615f7ddc4384c9acd4abfd3387216f69140f4e5e
Jonibek <94430009+MJony05@users.noreply.github.com>
2023-08-02 21:24:20 +0500

    search component

--- src/context/app.cotext.tsx
+++ src/context/app.cotext.tsx
@@ -3,32 +3,23 @@ import { MenuItem } from '../interfaces/menu.interface';
 import { PageCategory } from '../interfaces/page.interface';
 
 export interface IAppContext {
-  menu: MenuItem[];
-  firstCategory: PageCategory;
-  setMenu?: (newMenu: MenuItem[]) => void;
+	menu: MenuItem[];
+	firstCategory: PageCategory;
+	setMenu?: (newMenu: MenuItem[]) => void;
 }
 
-export const AppContext = createContext<IAppContext>({
-  menu: [],
-  firstCategory: PageCategory.Courses,
-});
+export const AppContext = createContext<IAppContext>({ menu: [], firstCategory: PageCategory.Courses });
 
-export const AppContextProvider = ({
-  firstCategory,
-  menu,
-  children,
-}: PropsWithChildren<IAppContext>): JSX.Element => {
-  const [menuState, setMenuState] = useState<MenuItem[]>(menu);
+export const AppContextProvider = ({ firstCategory, menu, children }: PropsWithChildren<IAppContext>): JSX.Element => {
+	const [menuState, setMenuState] = useState<MenuItem[]>(menu);
 
-  const setMenu = (newMenu: MenuItem[]) => {
-    setMenuState(newMenu);
-  };
-  useEffect(() => {
-    setMenu(menu);
-  }, [menu]);
-  return (
-    <AppContext.Provider value={{ menu: menuState, firstCategory, setMenu }}>
-      {children}
-    </AppContext.Provider>
-  );
+	const setMenu = (newMenu: MenuItem[]) => {
+		setMenuState(newMenu);
+	};
+
+	useEffect(() => {
+		setMenu(menu);
+	}, [menu]);
+
+	return <AppContext.Provider value={{ menu: menuState, firstCategory, setMenu }}>{children}</AppContext.Provider>;
 };
