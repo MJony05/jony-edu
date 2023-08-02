c72bdf2d10d752147940e3529ad119db39d68423
Jonibek <94430009+MJony05@users.noreply.github.com>
2023-08-02 21:36:43 +0500

    setting up NProgress

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -9,32 +9,33 @@ import { ScrollUp } from '../components';
 import { useRouter } from 'next/router';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
-	return (
-		<div className={styles.wrapper}>
-			<Header className={styles.header} />
-			<Sidebar className={styles.sidebar} />
-			<div className={styles.body}>{children}</div>
-			<Footer className={styles.footer} />
-			<ScrollUp />
-		</div>
-	);
+  return (
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+      <ScrollUp />
+    </div>
+  );
 };
 
-export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: FunctionComponent<T>) => {
-	return function withLayoutComponent(props: T): JSX.Element {
-		const router = useRouter();
-		console.log(router);
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(
+  Component: FunctionComponent<T>
+) => {
+  return function withLayoutComponent(props: T): JSX.Element {
+    const router = useRouter();
 
-		return (
-			<AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
-				{router.asPath === '/' ? (
-					<Component {...props} />
-				) : (
-					<Layout>
-						<Component {...props} />
-					</Layout>
-				)}
-			</AppContextProvider>
-		);
-	};
+    return (
+      <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
+        {router.asPath === '/' ? (
+          <Component {...props} />
+        ) : (
+          <Layout>
+            <Component {...props} />
+          </Layout>
+        )}
+      </AppContextProvider>
+    );
+  };
 };
