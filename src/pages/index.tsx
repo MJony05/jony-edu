ddcee6dc4a3134ed3eef1547de34586838b755c3
Jonibekjs <161560737+Jonibekjs@users.noreply.github.com>
2024-06-23 16:29:51 +0500

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
-import { GetServerSideProps } from 'next';
-import { MenuItem } from '../interfaces/menu.interface';
-import { withLayout } from '../layout/layout';
-import { HomePageComponent } from '../page-components';
-import Seo from '../layout/seo/seo';
+import axios from "axios";
+import { GetServerSideProps } from "next";
+import { MenuItem } from "../interfaces/menu.interface";
+import { withLayout } from "../layout/layout";
+import { HomePageComponent } from "../page-components";
+import Seo from "../layout/seo/seo";
 
 const Index = (): JSX.Element => {
   return (
@@ -12,23 +12,22 @@ const Index = (): JSX.Element => {
     </Seo>
   );
 };
-
+// sds
 export default withLayout(Index);
 
-export const getServerSideProps: GetServerSideProps<
-  HomePageProps
-> = async () => {
-  const { data: menu } = await axios.post<MenuItem[]>(
-    `${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find`,
-    { firstCategory: 0 }
-  );
+export const getServerSideProps: GetServerSideProps<HomePageProps> =
+  async () => {
+    const { data: menu } = await axios.post<MenuItem[]>(
+      `${process.env.NEXT_PUBLIC_DOMAIN}/api/page-find`,
+      { firstCategory: 0 }
+    );
 
-  return {
-    props: {
-      menu,
-    },
+    return {
+      props: {
+        menu,
+      },
+    };
   };
-};
 
 interface HomePageProps {
   menu: MenuItem[];
